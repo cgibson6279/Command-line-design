80ad17e380d432c6fbe8874484f31499ca0fe3f8
cgibson6279 <53957224+cgibson6279@users.noreply.github.com>
2021-02-24 15:49:46 -0500

    black -179 and mypy run to fix style and formatting

--- split.py
+++ split.py
@@ -17,6 +17,7 @@ import logging
 
 from typing import Iterator, List, Tuple
 
+
 def read_tags(path: str) -> Iterator[List[List[str]]]:
     with open(path, "r") as source:
         lines = []
@@ -31,29 +32,33 @@ def read_tags(path: str) -> Iterator[List[List[str]]]:
     if lines:
         yield lines
 
-def test_train_split(data:List[List[List[str]]], seed: int) -> Tuple[List[List[List[str]]], List[List[List[str]]], List[List[List[str]]]]:
-    '''
+
+def test_train_split(
+    data: List[List[List[str]]], seed: int
+) -> Tuple[List[List[List[str]]], List[List[List[str]]], List[List[List[str]]]]:
+    """
     :param data:
           Data = set of the format list of list of list of strings
     :return:
         train -> list strings representing train sample set
         dev -> list strings representing dev sample set
         test -> list strings representing test sample set
-    '''
+    """
 
-    #set training data length
+    # set training data length
     train_sample = int(len(data) * 0.8)
     dev_sample = int(len(data) * 0.1)
-    #shuffle data for sampling
+    # shuffle data for sampling
     random.Random(seed).shuffle(data)
-    #split data into test, train,split
+    # split data into test, train,split
     train = data[0:train_sample]
-    dev = data[train_sample: train_sample + dev_sample]
-    test = data[train_sample + dev_sample:]
+    dev = data[train_sample : train_sample + dev_sample]
+    test = data[train_sample + dev_sample :]
 
     return train, dev, test
 
-def get_metadata(data: List[List[List[str]]]) -> Tuple[int,int]:
+
+def get_metadata(data: List[List[List[str]]]) -> Tuple[int, int]:
     sents = 0
     tokens = 0
 
@@ -64,58 +69,74 @@ def get_metadata(data: List[List[List[str]]]) -> Tuple[int,int]:
 
     return sents, tokens
 
+
 def write_tags(data: List[List[List[str]]], output_path: str) -> None:
-    '''
+    """
     :param data:
         List of sent strings containing lists of words
     :param output_path:
         a path for which to write the data input
     :return:
         None
-    '''
+    """
 
-    with open(output_path, 'w', encoding = 'utf-8') as out_path:
-        #get word counts and write data to file
+    with open(output_path, "w", encoding="utf-8") as out_path:
+        # get word counts and write data to file
         for sent in data:
             for token in sent:
-                #convert list to string
-                word = ' '.join(token)
-                #write word to file
+                # convert list to string
+                word = " ".join(token)
+                # write word to file
                 out_path.write(f"{word}\n")
 
 
 def main(args: argparse.Namespace) -> None:
 
-    #read in corpus from input
+    # read in corpus from input
     corpus = list(read_tags(args.input))
 
-    #split data into test,train,split sets
+    # split data into test,train,split sets
     train, dev, test = test_train_split(corpus, args.seed)
 
-    #write data to file accordingly
+    # write data to file accordingly
     write_tags(train, args.train)
     write_tags(dev, args.dev)
     write_tags(test, args.test)
 
-    #log metadata about set
+    # log metadata about set
     train_sents, train_tokens = get_metadata(train)
     dev_sents, dev_tokens = get_metadata(dev)
     test_sents, test_tokens = get_metadata(test)
 
-    logging.info(f'Train sents: {train_sents}\nTrain tokens: {train_tokens}\n')
-    logging.info(f'Dev sents: {dev_sents}\nDev tokens: {dev_tokens}\n')
-    logging.info(f'Test sents: {test_sents}\nDev tokens: {test_tokens}\n')
+    logging.info(f"Train sents: {train_sents}\nTrain tokens: {train_tokens}\n")
+    logging.info(f"Dev sents: {dev_sents}\nDev tokens: {dev_tokens}\n")
+    logging.info(f"Test sents: {test_sents}\nDev tokens: {test_tokens}\n")
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description='Split some data in to train, dev split sets',
+        description="Split some data in to train, dev split sets",
     )
-    parser.add_argument('--seed', type=int, metavar='', required=True, help='Seed for random shuffle generation')
-    parser.add_argument("input", type=str, metavar='', help='Read all of the input data')
-    parser.add_argument('train', type=str, metavar='', help='Path for writing the training set')
-    parser.add_argument('dev', type=str, metavar='', help='Path for writing the dev set')
-    parser.add_argument('test', type=str, metavar='', help='Path for writing the test set')
+    parser.add_argument(
+        "--seed",
+        type=int,
+        metavar="",
+        required=True,
+        help="Seed for random shuffle generation",
+    )
+    parser.add_argument(
+        "input", type=str, metavar="", help="Read all of the input data"
+    )
+    parser.add_argument(
+        "train", type=str, metavar="", help="Path for writing the training set"
+    )
+    parser.add_argument(
+        "dev", type=str, metavar="", help="Path for writing the dev set"
+    )
+    parser.add_argument(
+        "test", type=str, metavar="", help="Path for writing the test set"
+    )
     logging.basicConfig(level=logging.INFO)
     namespace = parser.parse_args()
     main(namespace)
